00f9df6741bfb681c20e0ddddf116ca39a10e035
GulshatT <74847069+GulshatT@users.noreply.github.com>
2021-07-01 19:25:49 -0400

    added a login page (#8)

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,23 +2,11 @@ import React from 'react'
 
 const Nav = () => {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <p className="navbar-brand" href="#"></p>
-            <button className="navbar-toggler" onclick="movebutton()" data-toggle="collapse" data-target="#navbarMenu">
-            </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarMenu">
-              <ul className="navbar-nav">
-                    <li className="nav-item active">
-                    <a className="nav-link" >Log in</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" >Sign Up</a>
-                    </li>
-              </ul>
-            </div>
+        <nav class="nav">
+            <a href="#Login">Log In</a>
+            <a href="#Review">Review</a>
+            <a href="#Review">Favorites</a>
         </nav>
-        </div>
     )
 }
 
