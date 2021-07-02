76a8eb687bace8c40d9fd71eb36f8c12a87f314d
William Flores <floreswilliam891@hotmail.com>
2021-07-02 17:46:48 -0400

    added to app js and fixed routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
-import Login from './components/Login'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <>
-      <Navbar></Navbar>
-      <Homepage></Homepage>
-      <Login></Login>
-    </>
+    <Router>
+      <div>
+          <Navbar></Navbar>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Nav = () => {
     return (
         <nav class="nav">
-            <a href="#Login">Log In</a>
-            <a href="#Review">Review</a>
-            <a href="#Review">Favorites</a>
+            <a href="Login">Log In</a>
+            <a href="/">Main Menu</a>
+            <a href="Review">Review</a>
+            <a href="Review">Favorites</a>
         </nav>
     )
 }
