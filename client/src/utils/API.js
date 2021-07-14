1b2527019aea0add879a5113ac31c26102210b55
William Flores <floreswilliam891@hotmail.com>
2021-07-14 12:49:39 -0400

    quick Youtube fix

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,14 +2,10 @@ import axios from 'axios'
 
 const API = {
     getYoutubeResults: (type) => {
-<<<<<<< working-on-seeds
-        return axios.get(`/app/youtube/${type}`)
+        return axios.get(`/youtube/${type}`)
     },
     getQuotesResults: () => {
         return axios.get(`/api/quotes/`)
-=======
-        return axios.get(`/youtube/${type}`)
->>>>>>> main
     }
 }
 
