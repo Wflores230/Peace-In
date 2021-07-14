821e51f370235d44e3a843cab27edcc7408eb7e7
William Flores <floreswilliam891@hotmail.com>
2021-07-13 21:07:53 -0400

    fixing youtube api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const API = {
     getYoutubeResults: (type) => {
-        return axios.get(`/app/youtube/${type}`)
+        return axios.get(`/youtube/${type}`)
     }
 }
 
---
+++ controllers/quotesController.js
@@ -0,0 +1,37 @@
+const db = require("../models/quotes");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: function(req, res) {
+    db.Quotes
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Quotes
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Quotes
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Quotes
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Quotes
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
