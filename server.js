4244dbc6df103b1902cbe702a82aa7c441b2d0bd
William Flores <floreswilliam891@hotmail.com>
2021-07-14 21:37:43 -0400

    server tweak

--- server.js
+++ server.js
@@ -15,15 +15,16 @@ app.use(routes);
 // app.use(passport.initialize());
 // app.use(passport.session());
 // require("./passport")(passport);
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname,'build')));
-console.log(path.join(__dirname, 'build', 'index.html'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  });
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname,'build')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
+//   });
   
+// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
-
 
 // Connect to the Mongo DB
 mongoose.connect(
