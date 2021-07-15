b96ece47a1b1f9f6d1d427b998222fe40c4f828f
William Flores <floreswilliam891@hotmail.com>
2021-07-14 21:56:07 -0400

    adding login, quotes

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ const youtubeRoutes = require("./youtube")
 router.use("/youtube", youtubeRoutes);
 router.use('/api', apiRoutes);
 
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -15,13 +15,7 @@ app.use(routes);
 // app.use(passport.initialize());
 // app.use(passport.session());
 // require("./passport")(passport);
-// if(process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname,'build')));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'))
-//   });
-  
-// }
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
